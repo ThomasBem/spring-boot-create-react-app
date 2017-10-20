d6686fe5970acee49a5310e57f2b5387adfc8f8e
Thomas <thomas.bembridge@fmcti.com>
2017-10-20 19:44:42 +0200

    Bumped spring-boot to 1.5.7, bumped frontend to use React 16 and flow-bin 0.57.3

--- spring-boot-create-react-app-web/src/App.test.js
+++ spring-boot-create-react-app-web/src/App.test.js
@@ -2,8 +2,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import * as AppService from './AppService';
 
 it('renders Create-React-App logo', () => {
+  jest.spyOn(AppService, 'default').mockImplementationOnce(() => Promise.resolve('Spring-Boot-Create-React-App'));
   const app = shallow(<App />);
   expect(app.find('.App')).toHaveLength(1);
+  expect(app.find('Header')).toHaveLength(1);
 });
